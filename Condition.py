5fe978e73c6ab0301a2011e24ed78c5e737a2f1c
zake3 <zake3012@gmail.com>
2022-06-08 09:14:50 +0000

    first commit

---
+++ Condition.py
@@ -0,0 +1,15 @@
+mark = int(input('Please input you mark '))
+
+if mark>85:
+    print("Distinction")
+elif mark>65:
+    print("Pass")
+else:
+    print("FAIL sorry :(")
+
+if mark>85:
+    print("Distinction")
+if mark>65 and mark<85:
+    print("Pass")
+if mark<65:
+    print("FAIL sorry :(")
